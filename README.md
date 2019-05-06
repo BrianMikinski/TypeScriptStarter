36dd8241549157f51871ec21e471f8062d5ec9db
Brian Mikinski <bmikinski@basisplanning.com>
2019-05-06 18:07:30 -0500

    Update tsconfig, add examples

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 TypeScriptStart
 
 Basic Starter examples for running TypeScript code
+
+### Examples
+- Basic Typing
+- Interfaces
+- Traditional functions with typed parameters
+- Arrow Functions
+- Classes
+- Inheritance
+- protected, private, etc
--- index.ts
+++ index.ts
@@ -1,8 +1,71 @@
 
+// // standard variables
+// let variable:boolean = true;
 
-let test:boolean = true; // "hello world";
+// function adder(number1:number, number2:number): number{
 
-function adder(number1:number, number2:number): number{
+//     return number1 + number2 + 5;
+// }
 
-    return number1 + number2 + "hello world";
-}
+// // interfaces
+// interface User {
+//     firstName: string,
+//     lastName: string,
+//     isAdmin: boolean
+// }
+
+// var johnDoe:User = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     isAdmin: false
+// };
+
+// // arrow functions
+// let PrintUserName: (currentUser) => string;
+
+// PrintUserName = (testUser:User) => {
+
+//     let fullName = testUser.firstName + " " + testUser.lastName;
+
+//     console.log("Full Name: " + fullName);
+
+//     return fullName;
+// };
+
+// // classes
+// class SecurityUser {
+    
+//     Permissions: string;
+
+//     constructor(permissions:string) {
+//         this.Permissions = permissions; 
+//     }
+
+//     protected WritePermissions() {
+//         console.log("Permissions: " + this.Permissions);
+//     }
+// }
+
+// // inheritances
+// class ApplicationUser extends SecurityUser implements User {
+    
+//     firstName: string;
+//     lastName: string;
+//     isAdmin: boolean;
+
+//     constructor() {
+//         super("I'm an Admin!");
+//     }
+
+//     TestPermissions(){
+//         this.WritePermissions();
+//     }
+// }
+
+// let testApplicationuser = new ApplicationUser();
+
+// testApplicationuser.TestPermissions();
+
+// // console log
+// console.log("Test");
+// console.log("Adder: " + adder(5, 4));
